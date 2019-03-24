983e9e1c79337f6e116bc51d23e5880d24ea6c19
TheScriptan <skominas.ainis@gmail.com>
2019-03-24 06:21:11 +0100

    Initial commit

---
+++ app/src/main/java/com/example/mvvm_room_retrofit_sample/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mvvm_room_retrofit_sample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
